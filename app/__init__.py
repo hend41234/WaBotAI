2299a029ab0da44c78fc08f2d044c49241e109a6
Hend412434 <40081971+hend41234@users.noreply.github.com>
2023-12-30 00:18:19 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,43 +1,12 @@
-from fastapi import APIRouter, Request, Form
+from fastapi import FastAPI
+from app.api import app_router
 
-import datetime
+#-> init app ------------------------------------------
+app = FastAPI(
+    title="Whatsapp Bot",
+    version="0.0.1"
+)
 
-from app.api_model.resposemodel import ResponseModel
-from app.api.message.get_message import GetMessage
-from app.api.message.send_message import SendMessage
 
-app_router = APIRouter(prefix="/api")
-
-@app_router.post("/wa", response_model=ResponseModel)
-async def wa(request: Request):
-    """
-    POST request handler for the "/wa" endpoint.
-
-    Parameters:
-        request (Request): The incoming request object.
-        for try we need 3 fields: 
-            1. Body
-            2. To
-            3. From
-        3 fields are required and process the message
-        
-        with curl we can use this command:
-            curl -X POST -d Body="Hello" -d To="whatsapp:+14155238886" -d From="whatsapp:+6285869144649" http://localhost:8000/api/wa
-            
-
-    Returns:
-        ResponseModel: The response model containing the datetimestamp, message, user, and bot.
-    """
-    data = await request.form()
-    get_message = GetMessage(data)
-    create_message = SendMessage(get_message.from_message, get_message.to_message, get_message.message)
-    create_message.send_response()
-    
-    result = ResponseModel(
-        datetimestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        message=get_message.message,
-        user=get_message.from_message,
-        bot=get_message.to_message
-    )
-
-    return result
+#-> include router -----------------------------------
+app.include_router(app_router)
